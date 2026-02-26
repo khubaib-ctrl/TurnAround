fcec0d9c4a8a1494b5106a055952346f90edefe5
khubaib-ctrl <khubaib@homeeasy.com>
2026-02-26 12:47:24 +0500

    Add Tauri API modules and dialog plugin to cache with source maps

---
+++ .angular/cache/21.1.4/editgit/vite/deps/@tauri-apps_api_core.js
@@ -0,0 +1,27 @@
+import {
+  Channel,
+  PluginListener,
+  Resource,
+  SERIALIZE_TO_IPC_FN,
+  addPluginListener,
+  checkPermissions,
+  convertFileSrc,
+  invoke,
+  isTauri,
+  requestPermissions,
+  transformCallback
+} from "./chunk-3Q3OAZPQ.js";
+import "./chunk-GOMI4DH3.js";
+export {
+  Channel,
+  PluginListener,
+  Resource,
+  SERIALIZE_TO_IPC_FN,
+  addPluginListener,
+  checkPermissions,
+  convertFileSrc,
+  invoke,
+  isTauri,
+  requestPermissions,
+  transformCallback
+};
